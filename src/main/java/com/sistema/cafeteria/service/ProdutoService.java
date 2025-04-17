b22023b3ba317c98b4d53e9624d5d7eb163b5cf4
Rikelvem <rikelvemrsilva@gmail.com>
2025-04-17 12:33:50 -0300

    ✨ Criando a service

---
+++ src/main/java/com/sistema/cafeteria/service/ProdutoService.java
@@ -0,0 +1,29 @@
+package com.sistema.cafeteria.service;
+
+import com.sistema.cafeteria.model.Produto;
+import com.sistema.cafeteria.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository repository;
+
+    public ProdutoService(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Produto> listar() {
+        return repository.findAll();
+    }
+
+    public Produto salvar(Produto produto) {
+        return repository.save(produto);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
