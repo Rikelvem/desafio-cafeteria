76d43a335371d480182738dc7593af41d90fc1c7
Rikelvem <rikelvemrsilva@gmail.com>
2025-04-17 11:57:27 -0300

    🎉 Commit inicial

---
+++ src/test/java/com/sistema/cafeteria/CafeteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.cafeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
