2235c6f52e48cdec3dc49311568d28866d09476a
Rikelvem <rikelvemrsilva@gmail.com>
2025-04-22 14:39:01 -0300

    ✨ Criação de DTOs para melhora do projeto

---
+++ src/main/java/com/sistema/cafeteria/dto/request/ProdutoRequestDTO.java
@@ -0,0 +1,25 @@
+package com.sistema.cafeteria.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    private String descricao;
+
+    @NotNull(message = "Preço é obrigatório")
+    @Positive(message = "Preço deve ser maior que zero")
+    private Double preco;
+
+    @NotNull(message = "Categoria é obrigatória")
+    private Long categoriaId;
+
+}
---
+++ src/main/java/com/sistema/cafeteria/dto/response/ProdutoResponse.java
@@ -0,0 +1,15 @@
+package com.sistema.cafeteria.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProdutoResponse {
+
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+    private String categoriaNome;
+}
