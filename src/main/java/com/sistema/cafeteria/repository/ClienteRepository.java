6912ebf7aa7334523db1a079b249ee51e6a07133
Rikelvem <rikelvemrsilva@gmail.com>
2025-04-22 10:16:36 -0300

    ♻️ Adicionando parametros no repository

--- src/main/java/com/sistema/cafeteria/repository/ClienteRepository.java
+++ src/main/java/com/sistema/cafeteria/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.sistema.cafeteria.repository;
 
+import com.sistema.cafeteria.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository {
+public interface ClienteRepository extends JpaRepository <Cliente, Long> {
 }
